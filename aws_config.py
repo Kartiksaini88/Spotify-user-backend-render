91be00460e2d33f604e806b0cf0cf689c9762589
Kartik Saini <97525903+Kartiksaini88@users.noreply.github.com>
2025-11-07 20:47:57 +0530

    Reformat aws_config.py for consistency

--- aws_config.py
+++ aws_config.py
@@ -2,17 +2,18 @@ import boto3
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env (if available)
+
 load_dotenv()
 
-# Create DynamoDB connection
+
 dynamodb = boto3.resource(
     'dynamodb',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
     aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
-    region_name="ap-south-1"  # 👈 Must be inside quotes!
+    region_name="ap-south-1"  
 )
 
-# Reference your DynamoDB table
+
 users_table = dynamodb.Table('User')
+
 
